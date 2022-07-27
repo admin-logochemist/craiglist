426cfc47a8fd9e2ac7b99314c85758bd61cee42c
unknown <team@logochemist.com>
2022-07-27 13:19:54 +0500

    Gh

--- pages/FormSignup.js
+++ pages/FormSignup.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import styles from '../styles/form.module.css'
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { addDoc, collection, serverTimestamp, updateDoc, doc } from 'firebase/firestore'; 
+import { addDoc, collection, serverTimestamp, updateDoc, doc } from 'firebase/firestore';
 import { db, storage } from '../firebase';
 import { auth } from '../firebase';
 import Link from 'next/link';
@@ -25,82 +25,111 @@ function FormSignup() {
   const [bweb, setBweb] = useState("")
   const [uids, setUids] = useState("")
   const [hasError, Error] = useState("");
-  const [select,setSelect]= React.useState();
+  const [select, setSelect] = useState();
   const dispatch = useDispatch();
-  const stripePromise = loadStripe(`${process.env.STRIPE_PUBLIC_KEY}`)
   const router = useRouter()
-  const handleCapacity=(e)=>{
-      setSelect(e.target.value);
-     
+  const handleCapacity = (e) => {
+    setSelect(e.target.value);
+    
   }
-    const registers = async() => {
-
-        createUserWithEmailAndPassword(auth,email,password).then((userAuth) => {
-            setUids(userAuth.user.uid)
-            localStorage.setItem('email', email);
-            localStorage.setItem('accid', accId);
-             localStorage.setItem('displayName', name);
-        }).catch(function(error) {
-          var errorMessage = error.message;
-          console.log("errorMessage: "+ errorMessage)
-        });
-      
-        const stripe = await stripePromise;
-        const RegisterSession =
-         await axios.post('/api/connectedaccount',
-            {
-              
-                email: email,
-            
-            })
-            const accId= RegisterSession.data.id
-              console.log("accId: "+ accId)
-              const link=RegisterSession.data.link
-              console.log("link: "+ link)
-          
-  if(select==='User'|| accId!=null){
-          const docRef = await addDoc(collection(db, 'userid'), {
-              email:email,
-              password:password,
-              name:name,
-              phone:phone,
-              select:select,
-              bname:bname,
-            accId:accId,
-            object: "bank_account",
-          country: "US",
-          currency: "usd",
-          routing_number: "110000000",
-          account_number: "000123456789",
-          address:address   
-            })
-          
+  // const stripePromise = loadStripe(`${process.env.STRIPE_PUBLIC_KEY}`)
   
-            router.push(select==='user' ? '/' : link)
-          }
-     
-      // router.push(select==='user' ? '/' : '/ResturentOwner')
+  const registers = () => {
+
+    createUserWithEmailAndPassword(auth, email, password).then(async (userAuth) => {
+      setUids(userAuth.user.uid)
+      localStorage.setItem('email', email);
+      localStorage.setItem('displayName', name);
     
+
+    
+    if (select === 'User') {
+      console.log("slect value", select)
+
+      try{
+
+        const docRef = await addDoc(collection(db, 'userid'), {
+          email: email,
+          password: password,
+          name: name,
+          phone: phone,
+          select: select,
+          bname: bname,
+          
+          address: address
+        })
+        console.log("hellooooooooooo", docRef.id)
+    
+      }
+      catch(e){
+        console.log("error",e)
+      }
+      router.push("/")
     }
+
+
+    if (select === 'Seller') {
+      // const stripe = await stripePromise;
+
+      const RegisterSession =
+        await axios.post('/api/connectedaccount',
+          {
+
+            email: email,
+
+          })
+      const accId = RegisterSession.data.id
+      console.log("accId: " + accId)
+      localStorage.setItem('accid', accId);
+
+      const link = RegisterSession.data.link
+      console.log("link: " + link)
+      const docRef = await addDoc(collection(db, 'userid'), {
+        email: email,
+        password: password,
+        name: name,
+        phone: phone,
+        select: select,
+        bname: bname,
+        accId: accId,
+        object: "bank_account",
+        country: "US",
+        currency: "usd",
+        routing_number: "110000000",
+        account_number: "000123456789",
+        address: address
+      })
+
+
+
+      router.push(select === 'User' ? '/' : link)
+    }
+
+    // router.push(select==='user' ? '/' : '/ResturentOwner')
+  }).catch(function (error) {
+    var errorMessage = error.message;
+    console.log("errorMessage: " + errorMessage)
+  });
+  }
   return (
-    
-      <div className="login-page" id={styles.login_page}>
-        <div className="form" id={styles.form}>
-       
-            <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Enter Your Name" />
-            <input type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Enter Your Email" />
-            <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Your Password" />
-            <select name="" id="" onChange={(e) => setSelect(e.target.value)}>
-            <option value="">Select Account</option>
-            <option value="User">As User</option>
-            <option value="Seller">As Seller</option>
-            </select>
-            
-            <button onClick={registers}>create</button>
-       
-        </div>
+
+    <div className="login-page" id={styles.login_page}>
+      <div className="form" id={styles.form}>
+
+        <input type="text" onChange={(e) => setName(e.target.value)} placeholder="Enter Your Name" />
+        <input type="text" onChange={(e) => setEmail(e.target.value)} placeholder="Enter Your Email" />
+        <input type="password" onChange={(e) => setPassword(e.target.value)} placeholder="Enter Your Password" />
+        <select name="" id="" onChange={(e) => setSelect(e.target.value)}>
+          <option value="">Select Account</option>
+          <option value="User">As User</option>
+          <option value="Seller">As Seller</option>
+        </select>
+
+        <button onClick={registers}>create</button>
+
       </div>
-    
+    </div>
+
   )
 };
 
